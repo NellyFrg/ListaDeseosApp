67ed5045e89c5ec8ed2d20b4ad691432bec7b905
Nelly Fragoso Muñoz <nellyfrgoso1003@gmail.com>
2020-11-28 12:34:57 -0600

    Pantalla de pendientes

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -9,7 +9,14 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() { 
-    console.log('Servicio inicializado');
+
+    const lista1 = new Lista('Recolectar piedras del infinito');
+    const lista2 = new Lista('heroes a desaparecer');
+
+    this.listas.push(lista1, lista2);
+
+    
+
   }
 
 
